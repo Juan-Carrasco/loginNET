2cbab98e812b35dd9f1e7c76359de09ec2efb3b6
Juan-Carrasco <jca37@alumnos.unican.es>
2017-08-20 22:40:56 +0200

    Arreglados botones para que no funcoonen cuando no hay cambios

--- loginvuejs/index.js
+++ loginvuejs/index.js
@@ -17,29 +17,37 @@ $(document).ready(function(){
 				surname:"",
 				age:""
 			},
-			read: true
+			read: true,
+			menuChoice: "Personas"
 		}, 
-
-		methods:{
+		computed:{
 			disabledOnNoChange: function(){
 				retorno = false;
 				if(this.read){
 					retorno = true;
 				}
-				else if(!this.currentPerson.name || this.currentPerson.name.localeCompare(this.previousPerson.name) == 0){
+				else if(this.currentPerson.name != "" || this.currentPerson.name.localeCompare(this.previousPerson.name) == 0){
 					retorno = true;
 				}
-				else if(!this.currentPerson.surname || this.currentPerson.surname.localeCompare(this.previousPerson.surname) == 0){
+				else if(this.currentPerson.surname != "" || this.currentPerson.surname.localeCompare(this.previousPerson.surname) == 0){
 					retorno = true;
 				}
-				else if(!this.currentPerson.age || this.currentPerson.age.localeCompare(this.previousPerson.age) == 0){
+				else if(this.currentPerson.age != "" || this.currentPerson.age.localeCompare(this.previousPerson.age) == 0){
 					retorno = true;
 				}
-				return retorno	;
+				return retorno;
 
 			},
+		},
+
+		methods:{
+			chooseMenu: function(menu){
+				this.menuChoice = menu;
+				this.read = this.read; // Esto es para que se recargue menuChoice. No se por que
+				this.makeGetListRequest();
+			},
 			makeGetListRequest: function(){
-				$.ajax(url="http://localhost:57470/api/Personas",
+				$.ajax(url="http://localhost:57470/api/" + this.menuChoice,
 					method="GET")
 				.done(this.submitGetListValues)
 			},
@@ -47,7 +55,7 @@ $(document).ready(function(){
 				this.lista = datos;
 			},
 			makeGetRequest: function(id){
-				$.ajax(url="http://localhost:57470/api/Personas/" + id,
+				$.ajax(url="http://localhost:57470/api/" + this.menuChoice + "/" + id,
 					method="GET")
 				.done(this.submitDetailValues)
 			},
@@ -67,7 +75,7 @@ $(document).ready(function(){
 				}
 			},
 			makePutRequest: function(item){
-				$.ajax({url:"http://localhost:57470/api/Personas/" + item.index,
+				$.ajax({url:"http://localhost:57470/api/ " + this.menuChoice +"/" + item.index,
 					method:"PUT",
 					data: 	{Id: item.index,
 						Nombre: item.name,
@@ -79,15 +87,23 @@ $(document).ready(function(){
 				})
 			},
 			makePostRequest: function(item){
-				$.ajax({url:"http://localhost:57470/api/Personas",
+				$.ajax({url:"http://localhost:57470/api/" + this.menuChoice,
 					method:"POST",
 					data: 	{Nombre: item.name,
 						Apellido: item.surname,
 						Edad: item.age}})	
-				.done(this.makeGetListRequest)
+				.done(this.afterPostHandler)
 				.fail(function(){
 					alert("ELEMENTO NO CREADO");
 				})
+			},
+			afterPostHandler: function(datos){
+				this.makeGetListRequest();
+				this.currentPerson.index = datos.Id;
+				this.previousPerson.index = this.currentPerson.index;
+				this.previousPerson.name = this.currentPerson.name;
+				this.previousPerson.surname = this.currentPerson.surname;
+				this.previousPerson.age = this.currentPerson.age;
 			},
 			computedClass: function (index) {
 				classArray = "master-div-row ";
@@ -117,7 +133,7 @@ $(document).ready(function(){
 				this.currentPerson.age = "";
 			},
 			deleteItem: function(index){
-				$.ajax({url:"http://localhost:57470/api/Personas/" + index,
+				$.ajax({url:"http://localhost:57470/api/ " + this.menuChoice +"/" + index,
 					method:"DELETE"})	
 				.done(this.makeGetListRequest)
 				.fail(function(){
